d0aeff32968ecc9ca518b939162ad4b3019b0c97
paologhidoni <paolo.ghidoni82@gmail.com>
2025-12-02 14:21:02 +0100

    Add basic light and dark logos

--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -6,7 +6,7 @@ export default function RootLayout() {
     <div className="font-inter">
       <Navigation />
 
-      <main className="py-8 px-2 md:px-20 lg:px-25">
+      <main className="py-8 px-2 h-screen bg-(--color-bg-primary) text-(--color-text-primary) transition-colors transition-100md:px-20 lg:px-25  ">
         <Outlet />
       </main>
     </div>
