9a4cebd704c88df68ed75a784d42380826b39cc4
paologhidoni <paolo.ghidoni82@gmail.com>
2025-12-04 13:00:22 +0100

    Fetch individual game by id and do basic display of game info

--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -1,3 +1,30 @@
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router";
+import type { Game } from "../types";
+import { fetchGames } from "../util/http";
+
 export default function GameDetail() {
-  return <h1>Game Deatil</h1>;
+  const { id } = useParams();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["singleGame", { id }],
+    queryFn: ({ signal }) => fetchGames({ signal, query: { id } }),
+    staleTime: 5000,
+  });
+
+  return (
+    <>
+      {isPending && <p>Loading ....</p>}
+
+      {isError && <p>Error: {error.message}</p>}
+
+      {data && (
+        <>
+          <h1>{(data[0] as Game).name}</h1>
+
+          <img src={data[0].background_image} alt="" />
+        </>
+      )}
+    </>
+  );
 }
