f326fa1bd8535285831d4593af44f08b7762f9d6
paologhidoni <paolo.ghidoni82@gmail.com>
2025-12-06 07:37:46 +0100

    Simplify settings page

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,12 +1,6 @@
-import { useNavigate } from "react-router";
 import ThemeSelector from "../components/ThemeSelector";
-// import Button from "../components/Button";
-// import { useCallback } from "react";
 
 export default function Settings() {
-  const navigate = useNavigate();
-  // const handleNavigateBack = useCallback(() => navigate("/"), []);
-
   return (
     <div className="flex flex-col items-start gap-8">
       <h1 className="text-3xl font-bold text-(--color-text-primary)">
