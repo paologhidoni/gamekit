1c3c67d47017e098ef73c7560ffc9a8de3c7f74e
paologhidoni <paolo.ghidoni82@gmail.com>
2025-12-07 15:31:51 +0100

    Add custom fonts

--- src/components/SectionLabel.tsx
+++ src/components/SectionLabel.tsx
@@ -1,6 +1,6 @@
 export default function SectionLabel({ text }: { text: string }) {
   return (
-    <h2 className="md:text-lg rounded-2xl text-center font-bold py-2 px-4 bg-black text-(--color-accent-secondary) uppercase border-t-4 border-t-(--color-accent-primary)">
+    <h2 className="font-gonadaltes tracking-wider uppercase md:text-xl rounded-2xl text-center font-bold py-2 px-4 bg-black text-(--color-accent-secondary) border-t-4 border-t-(--color-accent-primary)">
       {text}
     </h2>
   );
