3a52e316110f7bbfd426bf8d44aae60ac4834a4e
paologhidoni <paolo.ghidoni82@gmail.com>
2025-12-08 17:09:45 +0100

    Data is now returned as single object, not array

--- src/pages/GameDetail.tsx
+++ src/pages/GameDetail.tsx
@@ -27,32 +27,32 @@ export default function GameDetail() {
           {/* HERO */}
           <section className="rounded-2xl p-4 border-y-2 border-y-black min-h-[30vh] max-h-[50vh] relative order-1 lg:order-0">
             <div className="absolute z-50 top-0 right-0 py-2 px-4 bg-black opacity-70 rounded-bl-2xl rounded-tr-2xl">
-              <GameRating rating={data[0].rating} />
+              <GameRating rating={data.rating} />
             </div>
 
             <div className="absolute z-50 bottom-4 left-0 bg-black opacity-70 rounded-r-2xl p-4 md:pl-6">
               <h1 className="text-2xl lg:text-4xl font-bold text-white wrap-break-words">
-                {data[0].name}
+                {data.name}
               </h1>
             </div>
 
             <BgImage
-              gameName={data[0].name}
-              gameBgImage={data[0].background_image}
+              gameName={data.name}
+              gameBgImage={data.background_image}
               extraClasses="object-top"
             />
           </section>
 
           {/* SIDEBAR */}
           <section className="bg-(--color-bg-secondary) rounded-2xl order-3 lg:order-0 lg:row-span-2 border-b-2 border-b-(--color-accent-secondary)">
-            <GameDetailSidebar game={data[0]} />
+            <GameDetailSidebar game={data} />
           </section>
 
           {/* DESCRIPTION */}
           <section className="bg-(--color-bg-secondary) rounded-2xl py-6 px-4 md:px-6 border-y-2 border-y-(--color-accent-secondary) order-2 lg:order-0">
             <p
               dangerouslySetInnerHTML={{
-                __html: data[0].description,
+                __html: data.description,
               }}
               className="leading-6.5"
             />
