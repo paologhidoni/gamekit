f9c9d6fdf175d6abcc42213ad3181f598c07e47e
paologhidoni <paolo.ghidoni82@gmail.com>
2026-03-22 11:44:11 +0100

    Refactor GameCard and BgImage as we introduced FavouriteButton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,6 +4,7 @@ import GameBadges from "./GenreBadges";
 import PlatformBadges from "./PlatformBadges";
 import GameRating from "./GameRating";
 import BgImage from "./BgImage";
+import FavouriteButton from "./FavouriteButton";
 
 interface GameCardProps {
   game: Game;
@@ -12,15 +13,14 @@ interface GameCardProps {
 
 export default function GameCard({ game, priority = false }: GameCardProps) {
   return (
-    <Link
-      to={`/game/${game.id}`}
-      className="flex flex-col gap-4 p-4 bg-(--color-bg-secondary) rounded-2xl group border-y-2 border-y-(--color-accent-primary) hover:border-y-(--color-accent-secondary) transition-all transition-100 hover:scale-105"
-    >
+    <article className="flex flex-col gap-4 p-4 bg-(--color-bg-secondary) rounded-2xl group border-y-2 border-y-(--color-accent-primary) hover:border-y-(--color-accent-secondary) transition-all transition-100 hover:scale-105">
       <div className="flex flex-col justify-between">
         <div className="flex justify-between items-center gap-2">
-          <h2 className="font-gonadaltes tracking-wider uppercase text-lg text-(--color-text-tertiary) font-bold mb-1 wrap-break-words">
-            {game.name}
-          </h2>
+          <Link to={`/game/${game.id}`} className="min-w-0 flex-1">
+            <h2 className="font-gonadaltes tracking-wider uppercase text-lg text-(--color-text-tertiary) font-bold mb-1 wrap-break-words">
+              {game.name}
+            </h2>
+          </Link>
 
           <GameRating rating={game.rating} />
         </div>
@@ -29,14 +29,26 @@ export default function GameCard({ game, priority = false }: GameCardProps) {
       </div>
 
       <div className="relative w-full h-64">
+        <Link
+          to={`/game/${game.id}`}
+          className="absolute inset-0 z-0 rounded-2xl"
+          aria-label={`View ${game.name}`}
+        />
+
         <BgImage
           gameName={game.name}
           gameBgImage={game.background_image}
           priority={priority}
         />
 
+        <FavouriteButton
+          gameId={game.id}
+          gameName={game.name}
+          className="absolute top-2 right-2 z-60"
+        />
+
         <GameBadges game={game} />
       </div>
-    </Link>
+    </article>
   );
 }
